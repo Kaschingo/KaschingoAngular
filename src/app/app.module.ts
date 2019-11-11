8886151f21b6c19db45e5849df7e0e44d2f9aeec
Steffen Exler <steffen.exler@pm.me>
2019-11-11 15:49:41 +0100

    refactor WagtailModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,40 +8,43 @@ import { WagtailModule } from 'angular-wagtail';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FooterComponent } from './footer/footer.component';
 
+const wagtailModuleSettings = {
+   pageTypes:
+      [
+         {
+            type: 'home.HomePage',
+            loadChildren: () => import('./home-page/home-page.module').then(m => m.HomePageModule)
+         },
+         {
+            type: 'catalog.ProductIndex',
+            loadChildren: () => import('./shop/product-index/product-index.module').then(m => m.ProductIndexModule)
+         },
+         {
+            type: 'catalog.Product',
+            loadChildren: () => import('./shop/product/product.module').then(m => m.ProductModule)
+         },
+         {
+            type: 'puput.BlogPage',
+            loadChildren: () => import('./blog/blog-page/blog-page.module').then(m => m.BlogPageModule)
+         },
+         {
+            type: 'puput.EntryPage',
+            loadChildren: () => import('./blog/entry-page/entry-page.module').then(m => m.EntryPageModule)
+         },
+      ],
+   wagtailSiteDomain: 'http://localhost:8000',
+};
+
 @NgModule({
    declarations: [
       AppComponent,
-      FooterComponent
+      FooterComponent,
    ],
    imports: [
       BrowserModule,
       AppRoutingModule,
-      WagtailModule.forRoot({
-         pageTypes: [
-            {
-               type: 'home.HomePage',
-               loadChildren: () => import('./home-page/home-page.module').then(m => m.HomePageModule)
-            },
-            {
-               type: 'catalog.ProductIndex',
-               loadChildren: () => import('./shop/product-index/product-index.module').then(m => m.ProductIndexModule)
-            },
-            {
-               type: 'catalog.Product',
-               loadChildren: () => import('./shop/product/product.module').then(m => m.ProductModule)
-            },
-            {
-               type: 'puput.BlogPage',
-               loadChildren: () => import('./blog/blog-page/blog-page.module').then(m => m.BlogPageModule)
-            },
-            {
-               type: 'puput.EntryPage',
-               loadChildren: () => import('./blog/entry-page/entry-page.module').then(m => m.EntryPageModule)
-            },
-         ],
-         wagtailSiteDomain: 'http://localhost:8000',
-      }),
-      NgbModule
+      NgbModule,
+      WagtailModule.forRoot(wagtailModuleSettings),
    ],
    providers: [],
    bootstrap: [AppComponent]
